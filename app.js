204b54cd64801c4be1f224e5f230c59946827783
rotem-ester <rotem.mazi@gmail.com>
2021-07-25 13:11:06 +0300

    inspect loop

--- app.js
+++ app.js
@@ -9,29 +9,66 @@ const databaseName = 'containers-loggs'
 
 const docker = new Docker()
 
-docker.listContainers((error, containers) => {
-    containers.forEach((container) => {
-        //console.log(`id: ${container.Id}, state: ${container.State}`)
-        console.log(container)
-        
+
+//for each running container: 
+//                              get container.Id
+//                              attach
+//                              get logs and follow
+//                              redirect all logs to DB
+
+
+
+
+
+const attachToRunningContainers = function (error, containers) {
+    if (error) {
+        return console.log(`error: ${error}`)
+    }
+    //filter the containers that are in 'running' state
+    const runningContainers = containers.filter((container) => {return container.State === 'running'})
+    let containersIds = []
+    let containerObjs = []
+    
+    runningContainers.forEach(container => {
+        containersIds.push(container.Id)
     })
-})
+
+    containersIds.forEach(id => {
+        containerObjs.push(docker.getContainer(id))
+    })
+
+    containerObjs.forEach(container => {
+        container.inspect((error, data) => {
+            console.log(data)
+        })
+    })
+
+    //console.log(containerObjs)
+}
+
+//list all containers
+docker.listContainers(attachToRunningContainers)
+
+
+
+
 
 // const container = docker.getContainer('54b19f592b6aea04243534405e22c7bc94a362947d08ac97bd32cafab16f71b0')
 // container.inspect((error, data) => {
-//     container.logs({stdout: true, strerr: true}, (error, result) => {
+//     container.logs({stdout: true, stderr: true}, (error, result) => {
 //         console.log(result.toString())
 //     })
 // })
 
-// const container1 = docker.getContainer('099eddbeef40c21c45a8a00512c32cbf73dfd3cb0d1586475c86af3d750088c5')
+// const container1 = docker.getContainer('818eec077b84dc1f0bbe7a876b99598d0b3288499aa3a4d5ddc0ae50b127645f')
 // container1.inspect((error, data) => {
-//     container1.logs({stdout: true, strerr: true, timestamps: true}, (error, result) => {
+//     container1.logs({stdout: true, stderr: true, timestamps: true}, (error, result) => {
 //         console.log(result.toString())
 //     })
 // })
+
 // container1.attach({stream: true, stdout: true, stderr: true}, (error, stream) => {
-//     container1.modem.demuxStream(stream, process.stdout, process.strerr)
+//     console.log(container1.modem.demuxStream(stream, process.stdout, process.stderr))
 // })
 
 
